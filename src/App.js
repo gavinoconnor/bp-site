ae83d15544aba807056e2b28a1c5b015d67a75d7
gavinoconnor <gedmund@gmail.com>
2020-02-18 18:20:09 -0500

    setup file structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import './App.css';
+
+import About from './components/About/About';
+import Clients from './containers/Clients/Clients';
+import Navbar from './containers/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          BP
-        </p>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <div className="App">
+        <header className="App-header">
+          <p>
+            <em>Hello. A creative leader from Brooklyn, Brian has been crafting and sharing the conceptual stories of consumer brands, professional services companies, and nonprofit organizations for over fifteen years. Request a full portfolio <Link to='/'>here.</Link></em>
+          </p>
+          <About />
+          <Clients />
+        </header>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/containers/Clients/Clients.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const clients = () => {
+  return(
+    <div>
+      <h2>Clients</h2>
+    </div>
+  );
+}
+
+export default clients;
