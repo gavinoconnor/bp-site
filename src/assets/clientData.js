54f23f2e22d6bdca6521fd75946ed3c2efebc026
gavinoconnor <gedmund@gmail.com>
2020-04-11 20:52:39 -0400

    add data

--- src/assets/clientData.js
+++ src/assets/clientData.js
@@ -4,13 +4,102 @@ const clientData = [
     image: 'nikon-1.jpg'
   },
   {
-    name: 'Nikon',
-    image: './images/nikon-1.jpg'
+    name: 'Berlin Rosen',
+    image: 'berlin-rosen.png'
   },
   {
-    name: 'Berlin-Rosen',
-    image: './images/berlin-rosen.png'
-  }
+    name: 'Anne Mülethaler',
+    image: 'moinian.png'
+  },
+  {
+    name: 'Piermont Bank',
+    image: 'nikon-2.jpg'
+  },
+  {
+    name: 'Nikon',
+    image: 'moinian.png'
+  },
+  {
+    name: 'eBay',
+    image: 'moinian.png'
+  },
+  {
+    name: 'HBO',
+    image: 'moinian.png'
+  },
+  {
+    name: 'Delta',
+    image: 'moinian.png'
+  },
+  {
+    name: 'Start With Why',
+    image: 'moinian.png'
+  },
+  {
+    name: 'goop',
+    image: 'moinian.png'
+  },
+  {
+    name: 'Motif',
+    image: 'nikon-1.jpg'
+  },
+  {
+    name: 'New York Life',
+    image: 'nikon-2.jpg'
+  },
+  {
+    name: 'FEED',
+    image: 'nikon-2.jpg'
+  },
+  {
+    name: 'Two Sigma',
+    image: 'nikon-2.jpg'
+    },
+  {
+    name: 'Over the Eight',
+    image: 'nikon-2.jpg'
+    },
+  {
+    name: 'LongLive',
+    image: 'nikon-2.jpg'
+    },
+  {
+    name: 'Third Rail Coffee',
+    image: 'nikon-2.jpg'
+    },
+  {
+    name: 'UEG',
+    image: 'nikon-2.jpg'
+    },
+  {
+    name: 'Origins School',
+    image: 'nikon-2.jpg'
+    },
+  {
+    name: 'BCG',
+    image: 'nikon-2.jpg'
+    },
+  {
+    name: 'White House Farm',
+    image: 'nikon-2.jpg'
+    },
+  {
+    name: 'the Green Building',
+    image: 'nikon-1.jpg'
+  },
+  {
+    name: 'CHA CHA\'s House',
+    image: 'nikon-1.jpg'
+  },
+  {
+    name: 'UNILEVER',
+    image: 'nikon-1.jpg'
+  },
+  {
+    name: 'Wise Guitar',
+    image: 'nikon-1.jpg'
+  },
+
 ]
 
 export default clientData;
