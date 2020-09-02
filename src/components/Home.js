618f66213c1d87c3dc32f3122128fbc7e844d325
gavinoconnor <gedmund@gmail.com>
2020-09-02 17:11:39 -0400

    prep to move to iPower

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,6 @@ const Home = () => {
       <p className='home-name'>Brian Ponto</p>
       <p className='home-text'>
         Hello. A creative leader from Brooklyn, Brian has been crafting and sharing the conceptual stories of consumer brands, professional services companies, and nonprofit organizations for over fifteen years. Request a full portfolio <a href='mailto:bp@brianponto.com?subject=Portfolio Request'>here.</a>
-        {/* TODO: link to form email with subject "Portfolio Request" */}
       </p>
     </div>
   );
